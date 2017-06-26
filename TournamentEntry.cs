03639ab6b0f633e41d242f0c1a564791a9f54049
w0otness <mathewtodd84@gmail.com>
2017-06-26 13:30:13 +1000

    First
    Git First Commit

---
+++ TournamentEntry.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.Scripts;
+using Assets.Scripts.Persistence;
+using BrilliantSkies.FromTheDepths.Planets;
+using UnityEngine;
+
+namespace w0otness
+{
+	public class TournamentEntry
+	{
+		public bool IsKing {get;set;}
+		public Tournament.SPAWN.DIR spawn_direction {get;set;}
+		public Tournament.SPAWN.LOC spawn_location {get;set;}
+		public ObjectId team_id {get;set;}
+		public float res {get;set;}
+		private BlueprintFile _bpf;
+		public BlueprintFile bpf
+		{
+			get
+			{
+				return _bpf;
+			}
+			set 
+			{
+				_bpf = value;
+				bp = bpf.Load();
+			}
+		}
+		public Blueprint bp;
+		public string[] labelCost
+		{
+			get
+			{
+				if (bp != null)
+				{
+					var scs = bp.SCs.FindAll(x => !x.IsSubConstructable());
+					var num = scs.Count;
+					if (num > 0)
+					{
+						var s = new string[num+1];
+						float smax = 0;
+						//float scur = 0;
+						for (int i = 0; i<num;i++)
+						{
+							float max = scs[i].CalculateResourceCost(false,true).Material;
+							s[i+1] = String.Format("{0} <color=cyan>{1}</color>",scs[i].blueprintName,max);
+							smax+=max;
+							//scur+=cur;
+						}
+						s[0] = String.Format("{0} <color=cyan>{1}</color>",bp.blueprintName,bp.CalculateResourceCost(false,true).Material-smax);
+						return s;
+					}
+					else
+					{
+						var s = new string[1];
+						s[0] = String.Format("{0} <color=cyan>{1}</color>",bp.blueprintName,bp.CalculateResourceCost(false,true).Material);
+						return s;
+					}
+				}
+				return null;
+			}
+		}
+		public string[] label
+		{
+			get
+			{
+				if (bp != null)
+				{
+					var scs = bp.SCs.FindAll(x => !x.IsSubConstructable());
+					var num = scs.Count;
+					if (num > 0)
+					{
+						var s = new string[num+1];
+						float smax = 0;
+						float scur = 0;
+						for (int i = 0; i<num;i++)
+						{
+							float max = scs[i].CalculateResourceCost(false,true).Material;
+							float cur = scs[i].CalculateResourceCost(true,true).Material;
+							s[i+1] = String.Format("{0} {1}",scs[i].blueprintName,Math.Round(cur/max*100,1));
+							smax+=max;
+							scur+=cur;
+						}
+						s[0] = String.Format("{0} {1}",bp.blueprintName,Math.Round((bp.CalculateResourceCost(true,true).Material-scur)/(bp.CalculateResourceCost(false,true).Material-smax)*100,1));
+						return s;
+					}
+					else
+					{
+						var s = new string[1];
+						s[0] = String.Format("{0} {1}",bp.blueprintName,Math.Round(bp.CalculateResourceCost(true,true).Material/bp.CalculateResourceCost(false,true).Material*100,1));
+						return s;
+					}
+				}
+				return null;
+			}
+		}
+
+		public void Spawn(float dis, float gap, int count, int pos) {//TODO
+			MainConstruct mainConstruct = BlueprintConverter.Convert(bp);
+			team_id = IsKing ? InstanceSpecification.i.Factions.Factions.Find(f => f.FactionSpec.Name == "KING").Id : InstanceSpecification.i.Factions.Factions.Find(f => f.FactionSpec.Name == "CHAL").Id;
+			BlueprintConverter.Initiate(mainConstruct, VLoc(gap,count,pos,dis), VDir(), team_id, null, SpawnPositioning.OriginOrCentre);
+			//SortedDictionary<int, TournamentParticipant> temp = new SortedDictionary<int, TournamentParticipant>();
+			//for 
+			//temp.Add(team_id, new TournamentParticipant
+			//{
+			//	TeamId = team_id,
+			//	TeamName = team_id.FactionSpec.AbreviatedName,
+			//	UniqueId = current.UniqueId,
+			//	BlueprintName = current.GetBlueprintName(),
+			//	AICount = current.BlockTypeStorage.MainframeStore.Blocks.Count,
+			//	HP = current.BlockTypeStorage.MainframeStore.Blocks.Count > 0 ? current.iMainStatus.GetFractionAliveBlocksIncludingSubConstructables() * 100 : current.iMainStatus.GetFractionAliveBlocks() * 100,
+			//	HPCUR = current.BlockTypeStorage.MainframeStore.Blocks.Count > 0 ? current.iMainStatus.GetNumberAliveBlocksIncludingSubConstructables() : current.iMainStatus.GetNumberAliveBlocks(),
+			//	HPMAX = current.BlockTypeStorage.MainframeStore.Blocks.Count > 0 ? current.iMainStatus.GetNumberBlocksIncludingSubConstructables() : current.iMainStatus.GetNumberBlocks()
+			//});
+		}
+		
+		public Vector3 VLoc(float gap, int count, int pos, float dis) {
+			switch (spawn_location) {
+				case Tournament.SPAWN.LOC.Sea:
+					return new Vector3((count - 1 * gap) / 2 - (pos * gap), 0, IsKing ? dis/2 : dis/2-dis);
+				case Tournament.SPAWN.LOC.Air:
+					return new Vector3((count - 1 * gap) / 2 - (pos * gap), 50, IsKing ? dis/2 : dis/2-dis);
+				case Tournament.SPAWN.LOC.Sub:
+					return new Vector3((count - 1 * gap) / 2 - (pos * gap), -10, IsKing ? dis/2 : dis/2-dis);
+				case Tournament.SPAWN.LOC.Land:
+					return new Vector3((count - 1 * gap) / 2 - (pos * gap), 51, IsKing ? dis / 2 : dis / 2 - dis);
+			}
+			return new Vector3((count - 1 * gap) / 2 - (pos * gap), 0, IsKing ? 500 : -500);
+		}
+		
+		public Quaternion VDir() {
+			switch (spawn_direction) {
+				case Tournament.SPAWN.DIR.Facing:
+					return Quaternion.LookRotation(new Vector3(0, 0, IsKing ? -1 : 1));
+				case Tournament.SPAWN.DIR.Away:
+					return Quaternion.LookRotation(new Vector3(0, 0, IsKing ? 1 : -1));
+				case Tournament.SPAWN.DIR.Left:
+					return Quaternion.LookRotation(new Vector3(IsKing ? 1 : -1, 0, 0));
+				case Tournament.SPAWN.DIR.Right:
+					return Quaternion.LookRotation(new Vector3(IsKing ? -1 : 1, 0, 0));
+			}
+			return Quaternion.LookRotation(new Vector3(0, 0, IsKing ? -1 : 1));
+		}
+		
+	}
+}
---
+++ TournamentPlugin.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using BrilliantSkies.FromTheDepths.Game;
+using BrilliantSkies.FromTheDepths.Planets;
+using UnityEngine;
+
+namespace w0otness
+{
+	public class TournamentPlugin:FTDPlugin
+	{
+		static Tournament _t;
+	    public void OnLoad()
+	    {
+	        //Debug.Log("Loading Tournament Stuff");
+	        _t = new Tournament();
+			GameEvents.PlanetChange += OnPlanetChange;
+			GameEvents.StartEvent += OnInstanceChange;
+	    }
+	    
+	    public void OnSave()
+	    {
+	    	
+	    }
+	    
+	    public string name
+	    {
+	        get { return "Tournament"; }
+	    }
+	    
+	    public static string Name
+	    {
+	        get { return "Tournament"; }
+	    }
+	    
+	    public Version version
+	    {
+	        get { return new Version("0.0.9"); }
+	    }
+	    
+	    public static void OnInstanceChange()
+	    {
+	    	//Debug.Log("TEST1 "+InstanceSpecification.i.Header.Name);
+			GameEvents.Twice_Second -= _t._me.SlowUpdate;
+			GameEvents.FixedUpdateEvent -= _t._me.FixedUpdate;
+			GameEvents.OnGui -= _t._me.OnGUI;
+			if (@is.Header.Name == InstanceSpecification.i.Header.Name)
+			{
+				_t._me._GUI.ActivateGui(_t._me);
+	    	}
+		}
+	    
+		static InstanceSpecification @is;
+		
+		public static void OnPlanetChange()
+		{
+			@is = new InstanceSpecification();
+			@is.Header.Name = "Tournament Creator";
+			@is.Header.Summary = "Create custom tournament style matches.";
+			@is.Header.Type = InstanceType.Designer;
+			@is.Header.CommonSettings.ConstructableCleanUp = ConstructableCleanUp.Off;
+			@is.Header.CommonSettings.HeartStoneRequirement = HeartStoneRequirement.None;
+			@is.Header.CommonSettings.BuildModeRules = BuildModeRules.Disabled;
+			@is.Header.CommonSettings.SavingOptions = SavingOptions.None;
+			@is.Header.CommonSettings.BlueprintSpawningOptions = BlueprintSpawningOptions.NoNewVehicles;
+			@is.Header.CommonSettings.EnemyBlockDestroyedResourceDrop = 0f;
+			@is.Header.CommonSettings.LocalisedResourceMode = LocalisedResourceMode.UseCentralStore;
+			@is.Header.CommonSettings.FogOfWarType = FogOfWarType.None;
+			@is.Header.CommonSettings.DesignerOptions = DesignerOptions.Off;
+			@is.Header.CommonSettings.LuckyMechanic = LuckyMechanic.Off;
+			@is.GenerateBlankInstance();
+			Planet.i.Designers.AddInstance(@is);
+			var kid = FactionSpecifications.i.AddNew(new FactionSpecificationFaction
+			{
+				Name = "KING",
+				AbreviatedName = "KING",
+				FleetColors = new Color[]
+				{
+					new Color(255f/255f,215f/255f,0f/255f,.5f),
+					new Color(218f/255f,165f/255f,32f/255f,.5f),
+					new Color(255f/255f,165f/255f,0f/255f,.5f),
+					new Color(218f/255f,140f/255f,0f/255f,.5f)
+				}
+			}).Id;
+			var cid = FactionSpecifications.i.AddNew(new FactionSpecificationFaction
+			{
+				Name = "CHAL",
+				AbreviatedName = "CHAL",
+				FleetColors = new Color[]
+				{
+					new Color(255f/255f,0f/255f,0f/255f,.5f),
+					new Color(139f/255f,0f/255f,0f/255f,.5f),
+					new Color(178f/255f,34f/255f,34f/255f,.5f),
+					new Color(255f/255f,99f/255f,71f/255f,.5f)
+				}
+			}).Id;
+			//Planet.i.Designers.GetInstance(@is.Header.Id).Factions.GetFaction(kid).eController = FactionController.None;
+			//Planet.i.Designers.GetInstance(@is.Header.Id).Factions.GetFaction(cid).eController = FactionController.AI_General;
+		}
+	}
+}
